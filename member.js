7c12ff5cd6fdf6c005cb89599c5ee5a85ff0dc8c
saurabhkolhatkar01 <saurabhkolhatkar01@gmail.com>
2023-07-22 15:39:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    member.style.display = "block";
+    skills.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    home.style.display = "none";
+}
